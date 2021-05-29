20fc8dc1b79596825167e2a62efd4f423fb05e94
Tim Primmer <timprimmer@gmail.com>
2021-05-29 14:57:09 -0500

    got functionality working, it generates a password based on criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,5 @@
 // Assignment code here
 
-/*
-  length,  8 - 128 Chars 
-  character types, lower upper num and/or special 
-  validate input, one char type selected
-*/
-
 // this function prompts the user and gets all of the required inputs with validation
 // returns this info as an object
 var getCriteria = function () {
@@ -66,6 +60,8 @@ var getCriteria = function () {
   return criteriaResults;
 };
 
+// function to generate clean numbers up to a given max
+// easier than writing out the math.random each time
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -76,33 +72,64 @@ var generatePassword = function () {
   var passReq = {};
   var tempPassword = "";
   var password = "default_password";
+  var randomSelection = "";
 
   passReq = getCriteria(); // Getting the password requirements via the get criteria function
 
-  for(x = 0; x < passReq.passLength; x++) {
-    console.log(getRandomInt(3));
-    console.log(password[x]);
+  // Because the user can select multiple criteria, the number of potential option combinations is quite vast
+  // I came up with this system to randomize all the slected options fairly
+  // I basically concat a string with the given numeric options (0,1,2, or 3) based on what the user selects
+  // then I randomly select one of the characters in this string, parse it as an int
+  // and use it in the switch below to randomly select an option with fair weighting for each
 
-    switch (getRandomInt(3)) {
-      case 0: // Generate random uppercase letter
-        break;
-      case 1:
-        break;
-      case 2:
-         break;
-      case 3:
-         break;
-    }
-
+  if (passReq.wantsLower === true) {
+    randomSelection += "0";
   }
-  /* 
-  passLength: 0,
-  wantsLower: false,
-  wantsUpper: false,
-  wantsNum: false,
-  wantsSpec: false
-  */     
 
+  if (passReq.wantsUpper === true) {
+    randomSelection += "1";
+  }
+
+  if (passReq.wantsNum === true) {
+    randomSelection += "2";
+  }
+
+  if (passReq.wantsSpec === true) {
+    randomSelection += "3";
+  }
+
+  for (x = 0; x < passReq.passLength; x++) {
+
+    // selecting a random character from the randomSelection string and parsing it as an int for the switch
+    switch (parseInt(randomSelection[getRandomInt(randomSelection.length)])) {
+
+      case 0: { // Generate random lowercase letter
+        // finding a random char in the alphabet variable and changing it to a lowercase
+        tempPassword += (alphabet[getRandomInt(alphabet.length)].toLowerCase());
+        break;
+      }
+
+      case 1: { // Generate random uppercase letter
+        // finding a random char in the alphabet variable and changing it to a lowercase
+        tempPassword += (alphabet[getRandomInt(alphabet.length)].toUpperCase());
+        console.log("upper");
+        break;
+      }
+
+      case 2: { // Generate random number
+        tempPassword += getRandomInt(9); // generates 0 - 9
+        break;
+      }
+
+      case 3: { // Generate random special character
+        // finding a random char in the specials variable
+        tempPassword += (specials[getRandomInt(specials.length)]);
+        break;
+      }
+    }
+  }
+  password = tempPassword;
+  tempPassword = "";
   return password;
 };
 
@@ -115,7 +142,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
